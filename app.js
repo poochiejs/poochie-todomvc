dac0c6e7c633fe9c8b2f0121438e866f62fc9f27
Greg Fitzgerald <garious@gmail.com>
2015-07-31 07:21:00 -0700

    Updated JavaScript

--- app.js
+++ app.js
@@ -270,9 +270,66 @@ function removeItem(idx, oTodoData) {
   oTodoData.set(data);
 }
 
+function isObservableFalse(o) {
+  return !o.get();
+}
+
+function isObservableTrue(o) {
+  return o.get();
+}
+
+// Return the value of the 'completed' field.
+function completedField(item) {
+  return item.completed;
+}
+
+// Return an array of only the value of the 'completed' field
+// from the input array.
+function completedFields(data) {
+  return data.map(completedField);
+}
+
+// Return an observable array where each item is an observable
+// indicating whether the item is complete or not. It is updated
+// if an item is marked complete or if the number of items in the
+// list changes.
+function getIsCompletedFields(oItems) {
+  // An observable of observables.
+  var oCompletedFields = oItems.map(completedFields);
+
+  // Notified if any of item changes state, or if the total number of items
+  // changes.
+  return observable.subscriber(oCompletedFields, function() {
+    return oCompletedFields.get();
+  });
+}
+
+// Return the number of observable items that are currently false.
+function getFalseCount(items) {
+  return items.filter(isObservableFalse).length;
+}
+
+// Return the number of observable items that are currently true.
+function getTrueCount(items) {
+  return items.filter(isObservableTrue).length;
+}
+
+// Return an observable containing the number of items left.
+function oGetItemsLeftCount(oItems) {
+  return getIsCompletedFields(oItems).map(getFalseCount);
+}
+
+// Return an observable containing the number of items completed.
+function oGetItemsCompletedCount(oItems) {
+  return getIsCompletedFields(oItems).map(getTrueCount);
+}
+
 module.exports = {
   addItem: addItem,
   createObservableTodoData: createObservableTodoData,
+  getIsCompletedFields: getIsCompletedFields,
+  oGetItemsLeftCount: oGetItemsLeftCount,
+  oGetItemsCompletedCount: oGetItemsCompletedCount,
   removeItem: removeItem
 };
 
@@ -327,7 +384,11 @@ function toggleCheckbox(text, oTodoData) {
 
   return dom.element({
     name: 'input',
-    attributes: {className: 'toggle-all', type: 'checkbox'},
+    attributes: {
+      className: 'toggle-all',
+      type: 'checkbox',
+      checked: model.oGetItemsLeftCount(oTodoData).map(not)
+    },
     style: {display: oTodoData.map(len).map(displayStyle)},
     handlers: {
       click: onClick
@@ -335,40 +396,15 @@ function toggleCheckbox(text, oTodoData) {
   });
 }
 
-function isObservableFalse(o) {
-  return !o.get();
-}
-
-function isObservableTrue(o) {
-  return o.get();
-}
-
-function todoItemsLeftContents(items) {
-  var itemsLeft = items.filter(isObservableFalse).length;
+function todoItemsLeftContents(itemsLeft) {
   return [
      dom.element({name: 'strong', contents: [String(itemsLeft)]}),
      ' item' + (itemsLeft === 1 ? '' : 's') + ' left'
   ];
 }
 
-function completedField(item) {
-  return item.completed;
-}
-
-function completedFields(data) {
-  return data.map(completedField);
-}
-
 function oTodoItemsLeftContents(oItems) {
-  // An observable of observables.
-  var oCompletedFields = oItems.map(completedFields);
-
-  // Notified if any of item changes state, or if the total number of items
-  // changes.
-  var oCompletedTodoData = observable.subscriber(oCompletedFields, function() {
-    return oCompletedFields.get();
-  });
-  return oCompletedTodoData.map(todoItemsLeftContents);
+  return model.oGetItemsLeftCount(oItems).map(todoItemsLeftContents);
 }
 
 function todoItemsLeft(oTodoData) {
@@ -500,25 +536,17 @@ function clearButton(s, oTodoData) {
     oTodoData.set(todoData);
   }
 
-  function visibleAttr(items) {
-    var itemsComplete = items.filter(isObservableTrue).length;
+  function visibleAttr(itemsComplete) {
     return itemsComplete > 0 ? 'visible' : 'hidden';
   }
-
-  // An observable of observables.
-  var oCompletedFields = oTodoData.map(completedFields);
-
-  // Notified if any of item changes state, or if the total number of items
-  // changes.
-  var oCompletedTodoData = observable.subscriber(oCompletedFields, function() {
-    return oCompletedFields.get();
-  });
 
   return dom.element({
     name: 'button',
     contents: [s],
     attributes: {className: 'clear-completed'},
-    style: {visibility: oCompletedTodoData.map(visibleAttr)},
+    style: {
+      visibility: model.oGetItemsCompletedCount(oTodoData).map(visibleAttr)
+    },
     handlers: {
       click: onClick
     }
@@ -549,7 +577,6 @@ module.exports = {
   todoHeader: function(xs) { return container(xs, 'header', 'header'); },
   todoItem: todoItem,
   todoItemsLeft: todoItemsLeft,
-  todoItemsLeftContents: todoItemsLeftContents,
   todoList: todoList,
   todoSection: function(xs) { return container(xs, 'section', 'todoapp'); },
   toggleCheckbox: toggleCheckbox,
